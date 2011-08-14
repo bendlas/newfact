1d340cc3e7cf184fe066db40ef5242e36c6e12bc
bendlas <hhochleitner@gmail.com>
2011-08-14 05:21:20 +0200

    Init project structure

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject newfact "0.0.1-SNAPSHOT"
+  :description "A refactoring tool for clojure code"
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]]
+
+  :aot [newfact.core])
---
+++ src/newfact/core.clj
@@ -0,0 +1,1 @@
+(ns newfact.core)
