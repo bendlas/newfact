132afe4e95a687e6a6341359abf0b2a15b025a21
bendlas <hhochleitner@gmail.com>
2011-08-14 08:54:07 +0200

    First part of analysis

--- src/newfact/core.clj
+++ src/newfact/core.clj
@@ -1,1 +1,26 @@
-(ns newfact.core)
+(ns newfact.core
+  (:require
+   [clojure.java.io :as io] 
+   (newfact
+    [reader :as r]
+    [find :as f])))
+
+(defn form-seq [rdr]
+  (lazy-seq
+   (let [form (r/read rdr false ::EOF false)]
+     (when-not (identical? form ::EOF)
+       (cons form (form-seq rdr))))))
+
+(defn rename [in n ref to]
+  (let [analysis
+        (with-open [rdr (r/push-back-reader (io/reader in))]
+          (reduce (fn [found form]
+                    (into found
+                          (map #(assoc (select-keys (meta %) [:start :end])
+                                  :sym %)
+                               (f/references form ref n))))
+                  []
+                  (form-seq rdr)))]
+    analysis))
+
+(rename "src/newfact/core.clj" *ns* 'rename 'rewuschel)
