d9b5ef0e7df46f31fc1e530eb61e3a8dfe84f88e
nfma <nfma@dell.(none)>
2011-09-14 20:55:59 +0200

    added deps and copied the mock functions from platogo

--- project.clj
+++ project.clj
@@ -5,13 +5,18 @@
                               ".." "lib" "tools.jar")))
 (defproject newfact "0.0.1"
   :description "A refactoring tool for clojure code"
+  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
+                 "platogo" "http://banana.platogo.com:8080/nexus/content/repositories/thirdparty/"}
+
   :dependencies [;; Core
                  [org.clojure/clojure "1.3.0-beta2"]
 
                  ;; Logging
                  [org.clojure/tools.logging "0.2.0"]
                  [ch.qos.logback/logback-classic "0.9.29"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
+		     [com.stuartsierra/lazytest "1.2.3" :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]
+		     [org.clojars.brenton/difform "1.0.2-PATCH" :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
 
   :extra-classpath-dirs [#=(tools-jar)])
